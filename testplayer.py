4d13bd02ba0eac6f040a1459ea61c21080c65baf
Johan Karlsson <johan.karlzone@gmail.com>
2021-04-13 18:22:54 +0200

    added testplayer test too

--- testplayer.py
+++ testplayer.py
@@ -3,43 +3,32 @@ import unittest
 
 class playertest(unittest.TestCase):
 
-    def testname4(self):
+    def testplayer1name(self):
         import Player
-        #Player.player.twoplayT(self)
+        # name = Player1
+        res = Player.player.Player1nameHard(self)
+        self.Player1name = res
+        exp = Player.player.Player1nameR(self)
+        self.assertEqual(res, exp)
+
+    def testplayer2name(self):
+        import Player
+        # name = Player2
         res = Player.player.Player2nameHard(self)
         self.Player2name = res
-        #exp = "Player2"
         exp = Player.player.Player2nameR(self)
         self.assertEqual(res, exp)
 
-    def Player2nameHard(self):
-        global Player2name
-#        Player2name = self.Player2name
-        Player2name = "Player2"
-        return Player2name
-    
-    def ctestname(self):
-        import Player
-        res = Player.player.namePlayer1(self)
-        exp = Player.player.Player1nameR(self)
 
-        self.assertEqual(res, exp)
-
-    def stestname2(self):
-        import Player
-        Player.player.twoplayT(self)
-
-        res = Player.player.namePlayer2(self)
-        exp = Player.player.Player2nameR(self)
-        self.assertEqual(res, exp)
-    
-    def stestname3(self):
+    def testAIname(self):
         import Player
         Player.player.twoplayF(self)
 
         res = Player.player.namePlayer2(self)
         exp = Player.player.Player2nameR(self)
         self.assertEqual(res, exp)
+
+
 
 if __name__ == '__main__':
 
